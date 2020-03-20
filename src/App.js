016e8b5e50e938828eececa6f21481cf168ce57c
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2020-03-20 16:17:28 -0600

    First load
    First load

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import React, { Component } from "react";
+import { render } from "@testing-library/react";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
