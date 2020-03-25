ddd615cfdc380c53e63be9ed317a1394afdb6048
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2020-03-25 17:50:46 -0600

    Updates

---
+++ precache-manifest.0a635483bbd34bfb0678200db89fe308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f78748a221ae929bd318c8010437109",
+    "url": "/udemyreact/index.html"
+  },
+  {
+    "revision": "78e0489ed9139d3b093b",
+    "url": "/udemyreact/static/css/main.7fee2647.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/udemyreact/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/udemyreact/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e0489ed9139d3b093b",
+    "url": "/udemyreact/static/js/main.04fb0277.chunk.js"
+  },
+  {
+    "revision": "f0002a1ba6bd6e7a69b8",
+    "url": "/udemyreact/static/js/runtime-main.da13e386.js"
+  }
+]);
